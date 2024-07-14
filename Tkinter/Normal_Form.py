3c21d8a8e762a57ec147536a8f82a137aa3ea034
Gorakh Bikram Baral <prayasbaral9800@gmail.com>
2024-07-14 22:31:17 +0545

    Form created using tkinter

---
+++ Tkinter/Normal_Form.py
@@ -0,0 +1,43 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("555x555")
+
+Label(root, text="Name").grid(pady=5,row=0)
+Label(root, text="Address").grid(pady=5,row=1)
+Label(root, text="Phone-Nb").grid(pady=5,row=2)
+Label(root, text="Height").grid(pady=5,row=4)
+Label(root, text="Weight").grid(pady=5,row=3)
+Label(root, text="Email").grid(pady=5,row=5)
+
+nameValue = StringVar()
+addressValue = StringVar()
+PhoneValue = IntVar()
+HeightValue = IntVar()
+WeightValue = IntVar()
+EmailValue = StringVar()
+
+nameEntry = Entry(root, textvariable=nameValue).grid(column=4, row=0,pady=5)
+addressEntry = Entry(root, textvariable=addressValue).grid(column=4, row=1,pady=5)
+PhoneEntry = Entry(root, textvariable=PhoneValue).grid(column=4, row=2,pady=5)
+HeightEntry = Entry(root, textvariable=HeightValue).grid(column=4, row=3,pady=5)
+WeightEntry = Entry(root, textvariable=WeightValue).grid(column=4, row=4,pady=5)
+EmailEntry = Entry(root, textvariable=EmailValue).grid(column=4, row=5,pady=5)
+
+def value():
+    with open("data.txt", "w") as f:
+        f.write(f"Name: {nameValue.get()}\n")
+        f.write(f"Address: {addressValue.get()}\n")
+        f.write(f"Phone: {PhoneValue.get()}\n")
+        f.write(f"Height: {HeightValue.get()}\n")
+        f.write(f"Weight: {WeightValue.get()}\n")
+        f.write(f"Email: {EmailValue.get()}\n")
+
+
+
+Button(root, text="Submit",command=value).grid(pady=5)
+
+
+
+
+root.mainloop()
