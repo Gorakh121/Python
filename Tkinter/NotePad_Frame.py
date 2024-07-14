c7943c442c74e360838befd15db2fd931615e387
Gorakh Bikram Baral <prayasbaral9800@gmail.com>
2024-07-14 13:44:32 +0545

    Button tag added

--- Tkinter/NotePad_Frame.py
+++ Tkinter/NotePad_Frame.py
@@ -3,6 +3,8 @@ from tkinter import *
 def rgb_to_hex(r, g, b):
     return f'#{r:02x}{g:02x}{b:02x}'
 
+color1 = '#262726'
+
 root = Tk()
 root.geometry("444x444")
 root.title("Notepad")
@@ -10,22 +12,48 @@ root.title("Notepad")
 f1 = Frame(root, bg=rgb_to_hex(45, 45, 44) , borderwidth=4)
 f1.pack( fill = X)
 
-l1 = Label(f1, text="File   Edit   View",
-bg = rgb_to_hex(45, 45, 44),
-fg = "white",
-font = ("times new roman",13))
-l1.pack(side=LEFT)
 
-f2 = Frame(root, bg="white")
+button1 = Button(f1 ,
+text = "File",
+bg = rgb_to_hex(45, 45, 44),
+activebackground = rgb_to_hex(45, 45, 44),
+activeforeground="white",
+fg = "white",
+borderwidth=0
+)
+button1.pack(side=LEFT, pady=2)
+button1.pack(padx=2)
+
+button2 = Button(f1 ,
+text="Edit",
+bg=rgb_to_hex(45, 45, 44),
+fg="white",
+activebackground = rgb_to_hex(45, 45, 44),
+activeforeground="white",
+borderwidth=0
+)
+button2.pack(side=LEFT, pady=2)
+button2.pack(padx=15)
+
+button3 = Button(f1 ,
+text="View",
+bg=rgb_to_hex(45, 45, 44),
+fg="white",
+activebackground = rgb_to_hex(45, 45, 44),
+activeforeground="white",
+borderwidth=0
+)
+button3.pack(side=LEFT, pady=2)
+button3.pack(padx=2)
+
+f2 = Frame(root)
 f2.pack( fill = X)
 f2.pack(fill = Y)
 
-l2 = Label(f2,\
-bg="white",
+l2 = Label(f2,
+bg= color1,
 fg="black",
 font=("times new roman",10))
 l2.pack()
-
-
 
 root.mainloop()
